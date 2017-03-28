39a4b22e7c20756ad61de6e76c95b4bb6583072c
KantaSunaga <kanta@sunagakanta-no-MacBook-Air.local>
2017-03-29 08:49:06 +0900

    biginerコントローラーにストロングパラメーターを追加

--- app/controllers/this_is_for_beginner_controller.rb
+++ app/controllers/this_is_for_beginner_controller.rb
@@ -4,17 +4,25 @@ class ThisIsForBeginnerController < ApplicationController
   end
 
   def result
-  # @result_serch_from_userparams = Bike.serch_bike_from_user_params(params[:bike_type],params[:detail_frame],params[:detail_bike_type],
-  #                                                                   params[:detail_component],params[:price_down],params[:price_up],
-  #                                                                   params[:color],params[:sex],params[:user_size_up],params[:user_size_down])
-  p "]]]]]]]]]]]]]]]]]]]]]]]]]"
-  p @result_serch_from_userparams = Bike.find_by( min_height: 100..160  )
-  # find_bik_from_sex_param(params[:sex]).find_bik_from_size_param(params[:user_size_up],params[:user_size_down])
+ serch_params = check_params_from_home
+  p serch_params
+  # p @result_serch_from_userparams = Bike.find_by( min_height: 100..160  )
+
   end
+
+  private
+    def check_params_from_home
+      params.permit(
+       :bike_type,
+       :frame_type,
+       :road_bike_type,
+       :component,
+       :price_down,
+       :price_up,
+       :color,
+       :sex,
+       :user_size_up,
+       :user_size_down)
+
+    end
 end
-# bike_type, frame_type,road_bike_type, component,
-#                                 price_down,price_up,color,sex,user_size_up,user_size_down)
-#
-#                                 "bike_type"=>"1", "detail_frame"=>"カーボン", "detail_bike_type"=>"", "detail_component"=>"",
-#                                  "price_down"=>"", "price_up"=>"",
-#                                  "color"=>"", "sex"=>"", "user_size_down"=>"150", "user_size_up"=>"155"
